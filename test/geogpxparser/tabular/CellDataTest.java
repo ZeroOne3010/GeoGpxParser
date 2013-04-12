159d4aa40e0a4ede77a194261537cc68c23c76ca
Ville Saalo <ville.saalo@iki.fi>
2013-04-13 02:24:51 +0300

    One new test case.

--- test/geogpxparser/tabular/CellDataTest.java
+++ test/geogpxparser/tabular/CellDataTest.java
@@ -18,4 +18,11 @@ public class CellDataTest {
         assertEquals("hello", cell.getText());
         assertEquals("http://www.example.com/foo", cell.getUrl().toString());
     }
+
+    @Test
+    public void malformed_url_should_leave_url_as_null() {
+        CellData cell = new CellData("hello", "omg : bbq ? foo / test");
+        assertEquals("hello", cell.getText());
+        assertNull(cell.getUrl());
+    }
 }
