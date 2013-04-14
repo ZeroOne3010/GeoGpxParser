d32979fac50a9106011da2cfe9387c03478c5e1f
Ville Saalo <ville.saalo@iki.fi>
2013-04-14 17:30:17 +0300

    Simplified the test by using a helper method or two.

--- test/geogpxparser/CacheListParserTest.java
+++ test/geogpxparser/CacheListParserTest.java
@@ -1,5 +1,6 @@
 package geogpxparser;
 
+import geogpxparser.tabular.CellData;
 import geogpxparser.tabular.TableData;
 import geogpxparser.tabular.TableRow;
 import java.util.List;
@@ -11,6 +12,10 @@ import org.junit.Test;
  */
 public class CacheListParserTest extends AbstractParserTest {
 
+    private static CellData gc(String code) {
+        return new CellData(code, "http://coord.info/" + code);
+    }
+
     @Test
     public void test_getTabularInfo() {
         TableData result = new CacheListParser().getTabularInfo(caches);
@@ -21,83 +26,22 @@ public class CacheListParserTest extends AbstractParserTest {
         assertEquals(6, rows.size());
 
         int row = 0;
-        assertTrue(rows.get(0).isHeader());
-        int col = 0;
 
-        assertEquals("gccode", rows.get(row).getCells().get(col++).getText());
-        assertEquals("type", rows.get(row).getCells().get(col++).getText());
-        assertEquals("name", rows.get(row).getCells().get(col++).getText());
-        assertEquals("longitude", rows.get(row).getCells().get(col++).getText());
-        assertEquals("latitude", rows.get(row).getCells().get(col++).getText());
-        assertEquals("size", rows.get(row).getCells().get(col++).getText());
-        assertEquals("difficulty", rows.get(row).getCells().get(col++).getText());
-        assertEquals("terrain", rows.get(row).getCells().get(col++).getText());
-        assertEquals("hidden", rows.get(row).getCells().get(col++).getText());
-        assertEquals("owner", rows.get(row).getCells().get(col++).getText());
+        assertEquals(headerRow("gccode", "type", "name", "longitude", "latitude", "size", "difficulty", "terrain", "hidden", "owner"), rows.get(row));
 
         row = 1;
-        col = 0;
-        assertEquals("GC111", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Traditional", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Cache I", rows.get(row).getCells().get(col++).getText());
-        assertEquals("1.1", rows.get(row).getCells().get(col++).getText());
-        assertEquals("2.2", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Micro", rows.get(row).getCells().get(col++).getText());
-        assertEquals("1.0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("5.0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("2001-01-02", rows.get(row).getCells().get(col++).getText());
-        assertEquals("John", rows.get(row).getCells().get(col++).getText());
+        assertEquals(row(gc("GC111"), "Traditional", "Cache I", "1.1", "2.2", "Micro", "1.0", "5.0", "2001-01-02", "John"), rows.get(row));
 
         row = 2;
-        col = 0;
-        assertEquals("GC222", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Multi", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Cache II", rows.get(row).getCells().get(col++).getText());
-        assertEquals("3.3", rows.get(row).getCells().get(col++).getText());
-        assertEquals("4.4", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Small", rows.get(row).getCells().get(col++).getText());
-        assertEquals("1.5", rows.get(row).getCells().get(col++).getText());
-        assertEquals("4.5", rows.get(row).getCells().get(col++).getText());
-        assertEquals("2002-03-04", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Mike", rows.get(row).getCells().get(col++).getText());
+        assertEquals(row(gc("GC222"), "Multi", "Cache II", "3.3", "4.4", "Small", "1.5", "4.5", "2002-03-04", "Mike"), rows.get(row));
 
         row = 3;
-        col = 0;
-        assertEquals("GC333", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Traditional", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Cache III", rows.get(row).getCells().get(col++).getText());
-        assertEquals("5.5", rows.get(row).getCells().get(col++).getText());
-        assertEquals("6.6", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Regular", rows.get(row).getCells().get(col++).getText());
-        assertEquals("2.0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("4.0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("2003-05-06", rows.get(row).getCells().get(col++).getText());
-        assertEquals("John", rows.get(row).getCells().get(col++).getText());
+        assertEquals(row(gc("GC333"), "Traditional", "Cache III", "5.5", "6.6", "Regular", "2.0", "4.0", "2003-05-06", "John"), rows.get(row));
 
         row = 4;
-        col = 0;
-        assertEquals("GC444", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Letterbox", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Cache IV", rows.get(row).getCells().get(col++).getText());
-        assertEquals("7.7", rows.get(row).getCells().get(col++).getText());
-        assertEquals("8.8", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Large", rows.get(row).getCells().get(col++).getText());
-        assertEquals("2.5", rows.get(row).getCells().get(col++).getText());
-        assertEquals("3.5", rows.get(row).getCells().get(col++).getText());
-        assertEquals("2004-07-08", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Mike", rows.get(row).getCells().get(col++).getText());
+        assertEquals(row(gc("GC444"), "Letterbox", "Cache IV", "7.7", "8.8", "Large", "2.5", "3.5", "2004-07-08", "Mike"), rows.get(row));
 
         row = 5;
-        col = 0;
-        assertEquals("GC555", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Mystery", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Cache V", rows.get(row).getCells().get(col++).getText());
-        assertEquals("9.9", rows.get(row).getCells().get(col++).getText());
-        assertEquals("10.1", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Not_chosen", rows.get(row).getCells().get(col++).getText());
-        assertEquals("3.0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("3.0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("2005-09-10", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Jake", rows.get(row).getCells().get(col++).getText());
+        assertEquals(row(gc("GC555"), "Mystery", "Cache V", "9.9", "10.1", "Not_chosen", "3.0", "3.0", "2005-09-10", "Jake"), rows.get(row));
     }
 }
