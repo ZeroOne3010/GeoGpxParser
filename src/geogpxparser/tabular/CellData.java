4accf8216fed4f7db3ea53674f271d2b0fb216fd
Ville Saalo <ville.saalo@iki.fi>
2013-04-14 16:40:37 +0300

    .equals(...) methods for CellData and TableRow.

--- src/geogpxparser/tabular/CellData.java
+++ src/geogpxparser/tabular/CellData.java
@@ -2,6 +2,7 @@ package geogpxparser.tabular;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,22 +12,24 @@ import java.util.logging.Logger;
 public class CellData {
     /** The text that's displayed in the cell. **/
     private final String text;
-    
+
     /** The URL to which the text should link to, if any. **/
     private final URL url;
 
-    public CellData(String text) {
-        this.text = text;
+    public CellData(String textParam) {
+        this.text = textParam;
         this.url = null;
     }
 
-    public CellData(String text, String url) {
-        this.text = text;
+    public CellData(String textParam, String urlParam) {
+        this.text = textParam;
         URL tempUrl = null;
-        try {
-            tempUrl = new URL(url);
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(CellData.class.getName()).log(Level.SEVERE, null, ex);
+        if (urlParam != null) {
+            try {
+                tempUrl = new URL(urlParam);
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(CellData.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         this.url = tempUrl;
     }
@@ -37,5 +40,32 @@ public class CellData {
 
     public URL getUrl() {
         return url;
+    }
+
+    @Override
+    public String toString() {
+        return "[Text: '" + text + "', URL: '" + url + "']";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof CellData)) {
+            return false;
+        }
+        CellData other = (CellData) o;
+        if ((other.getText() == null && text == null) || (other.getText() != null && other.getText().equals(text))) {
+            if ((other.getUrl() == null && url == null) || (other.getUrl() != null && other.getUrl().equals(url))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.text);
+        hash = 37 * hash + Objects.hashCode(this.url);
+        return hash;
     }
 }
--- src/geogpxparser/tabular/TableRow.java
+++ src/geogpxparser/tabular/TableRow.java
@@ -2,18 +2,19 @@ package geogpxparser.tabular;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * A class that represents a table row.
  */
 public class TableRow {
     private final boolean header;
-    private List<CellData> cells = new ArrayList<>();
+    private final List<CellData> cells = new ArrayList<>();
 
     public TableRow(boolean header) {
         this.header = header;
     }
-    
+
     public boolean isHeader() {
         return header;
     }
@@ -24,5 +25,35 @@ public class TableRow {
 
     public void addCell(CellData cell) {
         this.cells.add(cell);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof TableRow)) {
+            return false;
+        }
+        TableRow other = (TableRow) o;
+        if (other.getCells().size() != getCells().size()) {
+            return false;
+        }
+        if (other.isHeader() != isHeader()) {
+            return false;
+        }
+        for (int i = 0; i < cells.size(); i++) {
+            CellData thisCell = cells.get(i);
+            CellData otherCell = other.getCells().get(i);
+            if (!thisCell.equals(otherCell)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 13 * hash + (this.header ? 1 : 0);
+        hash = 13 * hash + Objects.hashCode(this.cells);
+        return hash;
     }
 }
