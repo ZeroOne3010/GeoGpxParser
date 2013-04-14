3194ad147eca447c8c007da1b9f49321b64f09f2
Ville Saalo <ville.saalo@iki.fi>
2013-04-14 16:57:11 +0300

    Simplified the test with a little helper function, now that the TableRow class has a working .equals() method.

--- test/geogpxparser/OwnerStatsParserTest.java
+++ test/geogpxparser/OwnerStatsParserTest.java
@@ -1,5 +1,6 @@
 package geogpxparser;
 
+import geogpxparser.tabular.CellData;
 import geogpxparser.tabular.TableData;
 import geogpxparser.tabular.TableRow;
 import java.util.List;
@@ -10,95 +11,45 @@ import org.junit.Test;
  * Tests for the OwnerStatsParser class.
  */
 public class OwnerStatsParserTest extends AbstractParserTest {
-    
+
+    private TableRow row(boolean header, String... cellValues) {
+        TableRow row = new TableRow(header);
+        for (String cellValue : cellValues) {
+            row.addCell(new CellData(cellValue));
+        }
+        return row;
+    }
+
+    private TableRow row(String... cellValues) {
+        return this.row(false, cellValues);
+    }
+
     @Test
     public void test_getTabularInfo() {
         // Assert that the test data has been initialized correctly:
         assertNotNull(caches);
         assertEquals(5, caches.size());
-        
+
         // Start testing:
         TableData result = new OwnerStatsParser().getTabularInfo(caches);
         assertNotNull(result);
-        
+
         List<TableRow> rows = result.getRows();
         assertNotNull(rows);
         assertEquals(4, rows.size());
-        
-        // Check the headers:
+
         int row = 0;
-        assertTrue(rows.get(0).isHeader());
-        int col = 0;
-        assertEquals("Owner", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Number of caches", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Number of cache types", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Traditional", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Multi", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Mystery", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Letterbox", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Event", rows.get(row).getCells().get(col++).getText());
-        assertEquals("EarthCache", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Virtual", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Webcam", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Wherigo", rows.get(row).getCells().get(col++).getText());
-        assertEquals("MegaEvent", rows.get(row).getCells().get(col++).getText());
-        assertEquals("CITO", rows.get(row).getCells().get(col++).getText());
-        assertEquals("Other", rows.get(row).getCells().get(col++).getText());
-        
-        // Check the data rows:
-        
+        assertEquals(row(true, "Owner", "Number of caches", "Number of cache types", "Traditional", "Multi", "Mystery",
+                "Letterbox", "Event", "EarthCache", "Virtual", "Webcam", "Wherigo", "MegaEvent", "CITO", "Other"),
+                rows.get(0));
+
         row = 1;
-        col = 0;
-        assertEquals("John", rows.get(row).getCells().get(col++).getText());
-        assertEquals("2", rows.get(row).getCells().get(col++).getText());
-        assertEquals("1", rows.get(row).getCells().get(col++).getText());
-        assertEquals("2", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        
+        assertEquals(row("John", "2", "1", "2", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"), rows.get(row));
+
         row = 2;
-        col = 0;
-        assertEquals("Mike", rows.get(row).getCells().get(col++).getText());
-        assertEquals("2", rows.get(row).getCells().get(col++).getText());
-        assertEquals("2", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("1", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("1", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
+        assertEquals(row("Mike", "2", "2", "0", "1", "0", "1", "0", "0", "0", "0", "0", "0", "0", "0"), rows.get(row));
 
         row = 3;
-        col = 0;
-        assertEquals("Jake", rows.get(row).getCells().get(col++).getText());
-        assertEquals("1", rows.get(row).getCells().get(col++).getText());
-        assertEquals("1", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("1", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
-        assertEquals("0", rows.get(row).getCells().get(col++).getText());
+        assertEquals(row("Jake", "1", "1", "0", "0", "1", "0", "0", "0", "0", "0", "0", "0", "0", "0"), rows.get(row));
     }
 }
