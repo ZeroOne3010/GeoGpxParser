f64f38fe5f09ff09065fddf97b04566df51ef7e3
Ville Saalo <ville.saalo@iki.fi>
2013-04-16 00:06:09 +0300

    Possibility to marshal tabular data into XML.

--- src/geogpxparser/tabular/TableData.java
+++ src/geogpxparser/tabular/TableData.java
@@ -2,17 +2,22 @@ package geogpxparser.tabular;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * A class that represents tabular data.
  */
+@XmlRootElement(name = "table")
 public class TableData {
+
     private List<TableRow> rows = new ArrayList<>();
-    
+
     public void addRow(TableRow row) {
         this.rows.add(row);
     }
-    
+
+    @XmlElement(name = "row")
     public List<TableRow> getRows() {
         return this.rows;
     }
