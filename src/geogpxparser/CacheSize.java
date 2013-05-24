ca058d12ee4af05711bf18b2d5d53ee3f1b66d4f
Ville Saalo <ville.saalo@iki.fi>
2013-05-25 02:14:24 +0300

    The CacheSize enum now knows what the values are called in the GPX files, so the GeoGPXParser class becomes a bit tidier.

--- src/geogpxparser/CacheSize.java
+++ src/geogpxparser/CacheSize.java
@@ -1,6 +1,34 @@
 package geogpxparser;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum CacheSize {
 
-    Micro, Small, Regular, Large, Not_chosen
+    Micro("Micro"),
+    Small("Small"),
+    Regular("Regular"),
+    Large("Large"),
+    Not_chosen("Not chosen");
+    private final String gpxDescription;
+    private static final Map<String, CacheSize> gpxToSize;
+
+    static {
+        gpxToSize = new HashMap<>();
+        for (CacheSize type : values()) {
+            gpxToSize.put(type.getGpxDescription(), type);
+        }
+    }
+
+    private CacheSize(String gpxDescriptionParam) {
+        this.gpxDescription = gpxDescriptionParam;
+    }
+
+    public String getGpxDescription() {
+        return this.gpxDescription;
+    }
+
+    public static CacheSize getByGpxDescription(String description) {
+        return gpxToSize.containsKey(description) ? gpxToSize.get(description) : Not_chosen;
+    }
 };
