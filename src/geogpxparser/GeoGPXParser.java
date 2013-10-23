240abfb82a70ad8b872e98e9cdf7df5abe42b2d5
Ville Saalo <ville.saalo@iki.fi>
2013-10-23 23:18:54 +0300

    Updated the years in the MIT license notices.

--- src/geogpxparser/GeoGPXParser.java
+++ src/geogpxparser/GeoGPXParser.java
@@ -1,5 +1,5 @@
 /*
- * The MIT License - Copyright (c) 2011-2012 Ville Saalo (http://coord.info/PR32K8V)
+ * The MIT License - Copyright (c) 2011-2013 Ville Saalo (http://coord.info/PR32K8V)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
