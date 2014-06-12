bd4257d536400043778ae42909fb6b63599979bf
Ville Saalo <ville.saalo@iki.fi>
2014-06-13 02:18:17 +0300

    Try-with-resources: closing the input stream automatically.

--- src/main/java/zeroone3010/geogpxparser/GeoGPXParser.java
+++ src/main/java/zeroone3010/geogpxparser/GeoGPXParser.java
@@ -123,8 +123,9 @@ public class GeoGPXParser {
     private void writeHtmlResources() throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
         for (String filename : new String[]{"jquery-1.9.1.min.js", "jquery.tablesorter.min.js"}) {
-            InputStream inputStream = classLoader.getResourceAsStream(filename);
-            Files.copy(inputStream, Paths.get(filename), StandardCopyOption.REPLACE_EXISTING);
+            try (InputStream inputStream = classLoader.getResourceAsStream(filename)) {
+                Files.copy(inputStream, Paths.get(filename), StandardCopyOption.REPLACE_EXISTING);
+            }
         }
     }
 
