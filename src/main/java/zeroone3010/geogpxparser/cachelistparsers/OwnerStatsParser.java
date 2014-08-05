57ae297bc6724030d9574979993e188acd3a986a
Ville Saalo <ville.saalo@iki.fi>
2014-08-05 23:12:33 +0300

    Refactored the common functionality of CountryStatsParser and OwnerStatsParser into a new, common parent class.

--- src/main/java/zeroone3010/geogpxparser/cachelistparsers/OwnerStatsParser.java
+++ src/main/java/zeroone3010/geogpxparser/cachelistparsers/OwnerStatsParser.java
@@ -1,15 +1,7 @@
 package zeroone3010.geogpxparser.cachelistparsers;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import zeroone3010.geogpxparser.CacheType;
 import zeroone3010.geogpxparser.Geocache;
 import zeroone3010.geogpxparser.tabular.CellData;
-import zeroone3010.geogpxparser.tabular.TableData;
-import zeroone3010.geogpxparser.tabular.TableRow;
 
 /**
  * Parses owner statistics from the given list of caches: the number of caches
@@ -17,121 +9,24 @@ import zeroone3010.geogpxparser.tabular.TableRow;
  *
  * @author Ville Saalo (http://coord.info/PR32K8V)
  */
-public class OwnerStatsParser implements ICachesToTabularDataParser {
-
-    private Map<String, Owner> owners = new LinkedHashMap<>();
-
-    /**
-     * Returns a String of the tabular format with data about cache owners.
-     *
-     * @param caches A List of Geocache objects
-     * @return A String that can be saved into a file and opened in a
-     * spreadsheet program.
-     */
+public class OwnerStatsParser extends AbstractCacheGroupStatsParser {
     @Override
-    public TableData getTabularInfo(List<Geocache> caches) {
-
-        // Parse cache owner info into a map:
-        for (Geocache cache : caches) {
-            addCacheForOwner(cache.getOwner(), cache.getType());
-        }
-
-        TableData result = new TableData("owners");
-
-        // Create titles:
-        TableRow headerRow = new TableRow(true);
-        headerRow.addCell(new CellData("Owner"));
-        headerRow.addCell(new CellData("Number of caches"));
-        headerRow.addCell(new CellData("Number of cache types"));
-        for (CacheType cacheType : CacheType.values()) {
-            headerRow.addCell(new CellData(cacheType.name()));
-        }
-        result.addRow(headerRow);
-
-        // Create data rows:
-        for (Owner owner : owners.values()) {
-            TableRow dataRow = new TableRow(false);
-            dataRow.addCell(new CellData(owner.getName(), Utility.getOwnerUrl(owner.getName())));
-            dataRow.addCell(new CellData(String.valueOf(owner.getTotalNumberOfCaches())));
-            dataRow.addCell(new CellData(String.valueOf(owner.getNumberOfCacheTypes())));
-
-            Map<CacheType, Integer> cacheMap = owner.getCaches();
-
-            for (Entry<CacheType, Integer> entry : cacheMap.entrySet()) {
-                dataRow.addCell(new CellData(String.valueOf(entry.getValue())));
-            }
-            result.addRow(dataRow);
-        }
-
-        return result;
+    String getTableId() {
+        return "owners";
     }
 
-    /**
-     * Initializes a new owner in the map if required, then adds +1 to the
-     * amount of the caches of the given type.
-     *
-     * @param owner Name of the cache owner
-     * @param cacheType Type of the cache
-     */
-    private void addCacheForOwner(String ownerName, CacheType cacheType) {
-        if (!owners.containsKey(ownerName)) {
-            owners.put(ownerName, new Owner(ownerName));
-        }
-
-        owners.get(ownerName).addCache(cacheType);
+    @Override
+    String getTableGroupColumnTitle() {
+        return "Owner";
     }
 
-    /**
-     * Represents someone who owns some geocaches. Keeps track of the name of
-     * the owner and the amount of different cache types they have.
-     */
-    private class Owner {
+    @Override
+    CellData createTableGroupColumnRowContent(Group group) {
+        return new CellData(group.getName(), Utility.getOwnerUrl(group.getName()));
+    }
 
-        private final String name;
-        private Map<CacheType, Integer> caches;
-
-        public Owner(String ownerName) {
-            name = ownerName;
-            caches = new LinkedHashMap<>();
-            for (CacheType cacheType : CacheType.values()) {
-                caches.put(cacheType, 0);
-            }
-        }
-
-        public void addCache(CacheType cacheType) {
-            int amount = caches.get(cacheType);
-            amount++;
-            caches.put(cacheType, amount);
-        }
-
-        public int getTotalNumberOfCaches() {
-            return caches.values().stream().reduce(0, Integer::sum);
-        }
-
-        public int getNumberOfCacheTypes() {
-            return (int) caches.values().stream().filter(val -> val > 0).count();
-        }
-
-        @Override
-        public int hashCode() {
-            return name.hashCode();
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public Map<CacheType, Integer> getCaches() {
-            return caches;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (o != null && !(o instanceof Owner)) {
-                return false;
-            }
-
-            return name != null && name.equals(((Owner) o).getName());
-        }
+    @Override
+    String getCacheGroupKey(Geocache cache) {
+        return cache.getOwner();
     }
 }
